380fab4e2ed0e9e705fda7b523fe02ef1b010967
lijilong0816 <591312849@qq.com>
2014-01-06 17:05:38 +0800

    25

--- douban/src/com/Lorin/douban/ReviewActivity.java
+++ douban/src/com/Lorin/douban/ReviewActivity.java
@@ -1,4 +1,4 @@
-package com.chenyc.douban;
+﻿package com.chenyc.douban;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,7 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.ContextMenu;
+import android.view.ContextMenu; 
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
